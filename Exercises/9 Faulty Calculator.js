a3440528fdfa93844a354d2e9ee2208e1696ac49
Manav Singla <manavsinghla123@gmail.com>
2025-04-08 00:28:04 +0530

    07/04/25

---
+++ Exercises/9 Faulty Calculator.js
@@ -0,0 +1,39 @@
+let a = Math.random() * 100;
+// let x = prompt("Enter 1st number: ");
+// let y = prompt("Enter 2nd number: ");
+let x = 10;
+let y = 20;
+
+// let z = prompt("Enter operator: ");
+let z = "+";
+
+switch (z) {
+  case '+':
+    if (a > 10) {
+      console.log(x + y);
+    } else {
+      console.log(x * y);
+    }
+    break;
+  case "-":
+    if (a > 10) {
+      console.log(x - y);
+    } else {
+      console.log(x / y);
+    }
+    break;
+  case "*":
+    if (a > 10) {
+      console.log(x * y);
+    } else {
+      console.log(x / y);
+    }
+    break;
+  case "/":
+    if (a > 10) {
+      console.log(x / y);
+    } else {
+      console.log(x ** y);
+      break;
+    }
+}
