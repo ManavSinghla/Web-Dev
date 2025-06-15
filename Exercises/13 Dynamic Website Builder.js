820175f6353c38b098fd643284a43cb18bb3be60
Manav Singla <manavsinghla123@gmail.com>
2025-06-15 16:23:12 +0530

    15-06-25

--- Exercises/13 Dynamic Website Builder.js
+++ Exercises/13 Dynamic Website Builder.js
@@ -58,9 +58,18 @@ function addCard(title, cName, views, monthsOld, duration, thumbnail){
     let p5=document.createElement("p");
     p5.innerHTML=monthsOld+" months ago";
     rightBelow.appendChild(p5);
+
+
+    // let html = `<div class="card">
+    // <div class="image">
+    //     <img src="${thumbnail}" alt="">
+    //     <div class="capsule">${duration}</div>
+    // </div>
+    // <div class="text">
+    //     <h1>${title}</h1>
+    //     <p>${cName} . ${viewStr} views . ${monthsOld} months ago</p>
+    //     </div>
+    // </div>`
+
+    // document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
 }
-
-
-addCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
-addCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
-addCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
